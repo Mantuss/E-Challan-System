86c5fc2717cb92a14460439ac8810e6d1d31febe
abhinavm07 <88830300+abhinavm07@users.noreply.github.com>
2022-03-27 21:20:31 +0545

    Abhinav Update

---
+++ user_traffic/adminPanel.js
@@ -0,0 +1,19 @@
+const dashboardEL = document.getElementById("dashboard-btn");
+const historyEL = document.getElementById("history-btn");
+const logoutEL = document.getElementById("logout-btn");
+
+dashboardEL.addEventListener("click", dashboard);
+historyEL.addEventListener("click", history);
+logoutEL.addEventListener("click", logout);
+
+function dashboard() {
+  console.log("Button Clicked");
+}
+
+function history() {
+  console.log("History Button clicked");
+}
+
+function logout() {
+  console.log("Bye Bitch");
+}
