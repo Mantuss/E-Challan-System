b47d77e8cc5fd404f196e6c4115e6b9df0c86212
rikesh mahat <np03cs4s210278@heraldcollege.edu.np>
2022-04-05 20:59:35 +0545

    updated admin UI

--- user_admin/Accounts.js
+++ user_admin/Accounts.js
@@ -1,21 +1,3 @@
-var rows = 12;
-var cols = 13;
-var value = "foo";
-var trafficData  = [...Array(rows).map(e => Array(cols).fill(value))];
-function loadTrafficData(){
-    for(var i  = 0; i < rows ; i++){
-        for (var j = 0; j < cols; j++){
-            console.log(trafficData[i][j]);
-        }
-    }
-}
-console.log("hello");
-
-loadTrafficData()
-
-function createTrafficTable(trafficData){
-   
-    
-    
-    // dataHTML = `<tr><td></td><td></td><td></td><td></td><td></td></tr>`
+function openPopUp(){
+    location.href = 'AddTraffic.html';
 }
