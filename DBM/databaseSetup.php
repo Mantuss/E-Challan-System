2f1884ade4ec10eeb1fcbc5b56ff800c18e9ac2f
Raymon Basnet <basnetraymonn@gmail.com>
2022-04-29 11:39:00 +0545

    Database Management Updated

--- DBM/databaseSetup.php
+++ DBM/databaseSetup.php
@@ -6,6 +6,7 @@ class SetupDatabase
     private $password;
     private $server;
     private $dbname;
+    private $mysqli;
 
     function __construct($username, $password, $server, $dbname)
     {
@@ -13,80 +14,108 @@ class SetupDatabase
         $this->password = $password;
         $this->server = $server;
         $this->dbname = $dbname;
+        $this->mysqli = new mysqli($this->server, $this->username, $this->password);
     }
 
-    function databaseCreate()
+    function ifExist()
     {
-        $conn = new mysqli($this->server, $this->username, $this->password);
 
-        if ($conn->connect_error) {
+        if ($this->mysqli->connect_error) {
+
             header("Location: http://localhost/E-Challan/Error_Pages/connection_lost.php");
             exit();
         }
 
-        $sql = "SHOW DATABASES LIKE '+$this->dbname+'";
+        $database = $this->mysqli->query("SHOW DATABASES LIKE '$this->dbname'");
+        $row = $database->fetch_assoc();
 
-        $exec = mysqli_query($conn, $sql);
+        if (!empty($row)) {
 
-        if (mysqli_num_rows($exec) == 0) {
+            return true;
+        } 
 
-            $sql = "CREATE DATABASE $this->dbname";
+        else {
 
-            if ($conn->query($sql)) {
-
-                $conn = new mysqli($this->server, $this->username, $this->password, $this->dbname);
-
-                $queries = array(
-
-                    "CREATE TABLE traffic_logs (
-                        traffic_id varchar(6) NOT NULL,
-                        traffic_post varchar(20) NOT NULL,
-                        account_status int NOT NULL,
-                        traffic_pass varchar(10) NOT NULL
-                    );",
-
-                    "CREATE TABLE challan (
-                        challan_id varchar(10) NOT NULL,
-                        license_number varchar(10) NOT NULL,
-                        vehicle_number varchar(10) NOT NULL, 
-                        user_name varchar(20) NOT NULL,
-                        traffic_id varchar(6) NOT NULL,
-                        time_stamp TIMESTAMP NOT NULL,
-                        date_time date NOT NULL,
-                        charge int NOT NULL
-                    );",
-
-                    "CREATE TABLE super_admin (
-                        admin_id varchar(20) NOT NULL,
-                        admin_pass varchar(10) NOT NULL,
-                        admin_status int NOT NULL
-                    );",
-
-                    "INSERT INTO super_admin(admin_id, admin_pass, admin_status) VALUES ('admin_traffic','admin_pass', 1);"
-                );
-
-                for ($i = 0; $i < sizeof($queries); $i++) {
-                    $conn->query($queries[$i]);
-                }
-
-                header("Location: ../user_admin/Login.html");
-                exit();
-                
-            } else {
-
-                header("Location: ../user_admin/LoginPanel.php");
-                exit();
-            }
-        } else {
-
-            header("Location: ../user_admin/LoginPanel.php");
-            exit();
+            return false;
         }
 
-        $conn->close();
+    }
+    function databaseCreate($url)
+    {
+        
+        if($this->ifExist()){
+
+            header("Location: http://localhost/E-Challan/user_admin/Login/Login.php");
+            exit();
+                    
+        }
+
+        if ($this->mysqli->query("CREATE DATABASE $this->dbname")) {
+
+            $conn = new mysqli($this->server, $this->username, $this->password, $this->dbname);
+            $queries = array(
+
+                "CREATE TABLE cms_traffic(
+                            traffic_id varchar(6) NOT NULL,
+                            account_status int NOT NULL,
+                            traffic_pass varchar(50) NOT NULL,
+                            firstname varchar(50) NOT NULL,
+                            lastname varchar(50) NOT NULL,
+                            email varchar(50) NOT NULL,
+                            number varchar(10),
+                            address varchar(50),
+                            state varchar(50),
+                            zip_code int(5),
+                            indicator int(10) NOT NULL
+                        );",
+
+                "CREATE TABLE cms_challan(
+                            challan_id varchar(10) NOT NULL,
+                            license_number varchar(10) NOT NULL,
+                            vehicle_number varchar(10) NOT NULL, 
+                            user_name varchar(20) NOT NULL,
+                            traffic_id varchar(6) NOT NULL,
+                            time_stamp TIMESTAMP NOT NULL,
+                            date_time date NOT NULL,
+                            charge int NOT NULL,
+                            cause int(10) NOT NULL
+                        );",
+
+                "CREATE TABLE cms_requests(
+                            request_id int(11) NOT NULL PRIMARY KEY,
+                            email varchar(50) NOT NULL,
+                            status int(10) NOT NULL
+                        );",
+
+                "CREATE TABLE cms_images(
+                            image_id int(11) NOT NULL PRIMARY KEY,
+                            images varchar(50) NOT NULL
+                        );",
+
+                "CREATE TABLE cms_admin (
+                            admin_id varchar(20) NOT NULL,
+                            admin_pass varchar(10) NOT NULL,
+                            admin_status int NOT NULL
+                        );",
+
+                "INSERT INTO cms_admin(admin_id, admin_pass, admin_status) VALUES ('admin_traffic','admin_pass', 1);",
+
+            );
+
+            foreach ($queries as $query) {
+                $conn->query($query);
+            }
+
+            header("Location: $url");
+            exit();
+
+        } else {
+            header("Location: http://localhost/E-Challan/Error_Pages/connection_lost.php");
+            exit();
+        }
     }
 }
 
 
-$setup = new SetupDatabase("root", "", "localhost", "echallan");
-$setup->databaseCreate();
+// $setup = new SetupDatabase("root", "", "localhost", "echall");
+// $setup->databaseCreate("http://localhost/E-Challan/user_admin/Login/Login.php");
